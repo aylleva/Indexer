4654f99ad6a4facbae6a2f065828e119e0792e56
ayleva <ayllayeva@gmail.com>
2024-10-22 10:31:11 +0400

    initial commit

---
+++ Arrayindexer/Arrayindexer/ListInt.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayindexer
+{
+    internal class ListInt
+    {
+        private int[] array;
+
+
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public ListInt()
+        {
+            array = new int[0];
+        }
+        public ListInt(int length)
+        {
+            array = new int[length];
+        }
+
+        public ListInt(params int[] nums)
+        {
+            array = nums;
+        }
+
+
+        public void Add(int num)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = num;
+
+        }
+        public void AddRange(params int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = nums[i];
+            }
+
+        }
+
+        public bool Contains(int num)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Sum()
+        {
+            int sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum+=array[i];
+               
+               
+              
+            }
+            Console.WriteLine(sum);
+        }
+        public void Remove(int num)
+        {
+            int[] newarray = new int[array.Length - 1];
+
+            int j = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != num)
+                {
+                    newarray[j] = array[i];
+                    j++;
+
+
+                }
+                newarray = array;
+               
+            }
+            return;
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Remove(nums[i]);
+            }
+        }
+        public override string ToString()
+        {
+            return string.Join(",", array);
+        }
+    }
+}
---
+++ Arrayindexer/Arrayindexer/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrayindexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt array = new ListInt(13, 15, 76, 89, 100, 54);
+
+            //array.AddRange(24, 65, 46);
+            //Console.WriteLine(array.Contains(100));
+
+            //array.Remove(13);
+
+            //array.RemoveRange(13, 15);
+
+            array.Sum();
+            Console.WriteLine(array.ToString());
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+        }
+    }
+}
